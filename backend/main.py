533c1f31d1b16eb890e744c2479d9808d6bdb117
qodize <vladislav.gusev.dev@gmail.com>
2022-11-18 22:26:41 +0300

    main initial nginx check

---
+++ backend/main.py
@@ -0,0 +1,15 @@
+import flask as fl
+from flask_cors import CORS
+
+
+app = fl.Flask(__name__)
+CORS(app)
+
+
+@app.route('/index')
+def index():
+    return 'hello'
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8083)
