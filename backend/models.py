dab6f17a1394b9ce239e4ba51c105f60d682a9f0
qodize <vladislav.gusev.dev@gmail.com>
2022-11-18 23:50:21 +0300

    sqlalchemy user password hash

--- backend/models.py
+++ backend/models.py
@@ -1,4 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
+from passlib.apps import custom_app_context as pwd_context
+
 
 db = SQLAlchemy()
 
@@ -10,6 +12,14 @@ class User(db.Model):
     first_name = db.Column(db.String(100), nullable=False)
     second_name = db.Column(db.String(100), nullable=False)
     father_name = db.Column(db.String(100), default='')
+    status = db.Column(db.String(100), default='pending')
+    password_hash = db.Column(db.String(1000), nullable=False)
+
+    def hash_password(self, password):
+        self.password_hash = pwd_context.encrypt(password)
+
+    def verify_password(self, password):
+        return pwd_context.verify(password, self.password_hash)
 
     def __repr__(self):
         return f'User<{self.id}>'
