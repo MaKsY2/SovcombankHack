71d3b76e70bf7447a0c31e008f8dabd41bfbd2d8
qodize <vladislav.gusev.dev@gmail.com>
2022-11-20 07:25:24 +0300

    decimal -> int

--- backend/app.py
+++ backend/app.py
@@ -320,6 +320,7 @@ def currencies_handler():
 def currency_handler():
     from_tag = fl.request.args.get('from_tag', 'RUB')
     to_tag = fl.request.args.get('to_tag', 'RUB')
+    print(from_tag, to_tag)
     from_currency = Currency.query.filter_by(tag=from_tag).first()
     to_currency = Currency.query.filter_by(tag=to_tag).first()
     if not from_currency or to_currency:
